e1d9b7b00296631219b827ccec619d2186f3df4d
Vaibhav20777 <narayanvaibhav212@gmail.com>
2026-03-29 00:40:46 +0100

    bind to 0.0.0.0 port 5000

--- app.py
+++ app.py
@@ -46,5 +46,4 @@ def analyse():
 
     return jsonify(result)
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=5000)
