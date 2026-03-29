ebedf9b6362b9fdf10943029208c9d494dbe5d49
Vaibhav20777 <narayanvaibhav212@gmail.com>
2026-03-29 01:18:14 +0100

    remove duplicate code

--- app.py
+++ app.py
@@ -15,35 +15,41 @@ def home():
     return render_template("index.html")
 @app.route("/analyse",methods=["POST"])
 def analyse():
-    file = request.files["cv"]
-    pdf = fitz.open(stream=file.read(),filetype="pdf")
-    text =""
-    for page in pdf:
-        text += page.get_text()
-    job_description = request.form.get("job_description","")
-    job_section = f"Job Description:\n{job_description}" if job_description else ""
-    prompt = f"""
-    Analyse this CV and return ONLY a JSON object with no explanation:
-    {{
-        "score": 85,
-        "summary": "summary here",
-        "strengths": ["strength 1", "strength 2"],
-        "improvements": ["improvement 1", "improvement 2"],
-        "job_match" : null
-    }}
-    {job_section}
+    try:
+        file = request.files["cv"]
+        pdf = fitz.open(stream=file.read(),filetype="pdf")
+        text =""
+        for page in pdf:
+            text += page.get_text()
+        job_description = request.form.get("job_description","")
+        job_section = f"Job Description:\n{job_description}" if job_description else ""
+        prompt = f"""
+        Analyse this CV and return ONLY a JSON object with no explanation:
+        {{
+            "score": 85,
+            "summary": "summary here",
+            "strengths": ["strength 1", "strength 2"],
+            "improvements": ["improvement 1", "improvement 2"],
+            "job_match" : null
+        }}
+        {job_section}
 
-    CV:
-    {text}
-    """
-    response = client.chat.completions.create(
-       model = "meta-llama/llama-2-7b-chat:free",
-        messages=[{"role": "user","content":prompt}]
-    )
-    raw = response.choices[0].message.content
-    raw = raw.strip().replace("```json", "").replace("```", "").strip()
-    result = json.loads(raw)
+        CV:
+        {text}
+        """
+        response = client.chat.completions.create(
+            model="meta-llama/llama-2-7b-chat:free",
+            messages=[{"role": "user","content":prompt}]
+        )
+        raw = response.choices[0].message.content
+        raw = raw.strip().replace("```json", "").replace("```", "").strip()
+        result = json.loads(raw)
+        return jsonify(result)
+    except Exception as e:
+        import traceback
+        print(traceback.format_exc())
+        return jsonify({"error": str(e)}), 500
 
-    return jsonify(result)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
